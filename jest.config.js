0cd3a72aff65b0a62cc5755bfb676b68a888704a
Maxich <wimer2630@gmail.com>
2023-08-18 11:14:05 +0400

    jest-html-reporters

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 const nextJest = require('next/jest')
+const path = require('path')
 
 const createJestConfig = nextJest({
   dir: './',
@@ -8,7 +9,17 @@ const customJestConfig = {
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: [
     '<rootDir>/jest.setup.js'
- ]
+ ],
+  reporters: [
+    "default",
+    [
+      "jest-html-reporters", 
+      {
+        publicPath: path.resolve(__dirname, "reports"),
+        filename: "unitTestsReport.html"
+      }
+    ]
+  ]
 }
 
 module.exports = createJestConfig(customJestConfig)
