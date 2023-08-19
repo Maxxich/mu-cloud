e0988518dadce096c34d7c684eac91b3804676d5
Maxich <wimer2630@gmail.com>
2023-08-19 13:35:33 +0400

    loader ui

---
+++ src/shared/ui/Loader/Loader.stories.tsx
@@ -0,0 +1,43 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Loader } from './Loader'
+
+const meta = {
+	title: 'shared/Loader',
+	component: Loader,
+	parameters: {
+		layout: 'centered',
+	},
+	tags: ['autodocs'],
+} satisfies Meta<typeof Loader>
+
+export default meta
+type Story = StoryObj<typeof meta>;
+
+const args: Story['args'] = {
+}
+
+export const MSize: Story = {
+	args
+}
+
+export const LSize: Story = {
+	args: {
+        ...args,
+        size: 'l'
+    }
+}
+
+
+export const XLSize: Story = {
+	args: {
+		...args,
+		size: 'xl'
+	}
+}
+
+export const Centered: Story = {
+	args: {
+		...args,
+		center: true
+	}
+}
