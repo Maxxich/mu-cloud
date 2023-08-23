9e14837f9f2f39d88d4cd6667d5283c1188c6899
Maxich <wimer2630@gmail.com>
2023-08-23 18:50:17 +0400

    SVGR fix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: { 
+        remotePatterns: [
+            {
+              protocol: 'http',
+              hostname: 'localhost',
+              port: '5001',
+              pathname: '/**',
+            },
+          ],
+    },
+    webpack: (config) => {
+      config.module.rules.push({
+        test: /\.svg$/i,
+        use: ['@svgr/webpack'],
+      });
+      return config;
+    },
+}
 
 module.exports = nextConfig
