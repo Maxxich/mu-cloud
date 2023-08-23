bd730159ed1d9772a4bf9e360f32d9dba2172343
Maxich <wimer2630@gmail.com>
2023-08-24 00:02:51 +0400

    fix getSelectedTrack

--- src/entity/player/model/selectors/getSelectedTrack.ts
+++ src/entity/player/model/selectors/getSelectedTrack.ts
@@ -3,6 +3,7 @@ import { StateSchema } from '@/global/providers/StoreProvider';
 export const getSelectedTrack = (state: StateSchema) => {
     const id = state.player.currentTrackId
     const list = state.player.list
+    if (!list) return undefined
     const track = list.find(t => t.id === id)
     return track
 }
