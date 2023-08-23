ad12272cc5f52260b78d13eb3aee120befa18a18
Maxich <wimer2630@gmail.com>
2023-08-24 00:03:26 +0400

    fix MobilePlayerContorls.tsx

--- src/feature/MobilePlayerContorls/ui/MobilePlayerContorls.tsx
+++ src/feature/MobilePlayerContorls/ui/MobilePlayerContorls.tsx
@@ -6,18 +6,29 @@ import { useAppDispatch } from '@/global/providers/StoreProvider/config/store';
 import { StateSchema } from '@/global/providers/StoreProvider';
 import { openPlayer } from '@/entity/player';
 import { RightAddon } from './RightAddon';
+import { useEffect } from 'react'
 import { TogglePause } from './TogglePause';
 import cls from './PlayerMobile.module.scss'
 import { Track } from '@/entity/track';
+import { useAudio } from '@/shared/lib/useAudio/useAudio';
 
 interface PlayerProps {
     selectedTrack: Track
     className?: string;
 }
 
-export const MobilePlayerContorls = memo((props: PlayerProps) => {
+export const MobilePlayerContorls = memo(({
+    selectedTrack
+}: PlayerProps) => {
     const dispatch = useAppDispatch()
-    const {list, currentTrackId} = useSelector((state: StateSchema) => state.player)
+
+    // const audio = useAudio()
+
+    // useEffect(() => {
+        
+    // }, [selectedTrack])
+
+    // console.log(audio)
     
     return (
         <div className={cls.container}
@@ -25,7 +36,7 @@ export const MobilePlayerContorls = memo((props: PlayerProps) => {
         >
             <TogglePause/>
             <div className={cls.info}>
-                <span className={cls.trackname}>{list?.[0]?.name}</span>
+                <span className={cls.trackname}>{selectedTrack.name}</span>
                 <span className={cls.author}>zxc</span>
             </div>
             <RightAddon/>
