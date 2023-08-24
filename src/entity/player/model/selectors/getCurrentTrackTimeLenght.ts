7bdcdab1f0853d1178c08e85dc684cb88193f090
Maxich <wimer2630@gmail.com>
2023-08-24 22:08:22 +0400

    player selectors

---
+++ src/entity/player/model/selectors/getCurrentTrackTimeLenght.ts
@@ -0,0 +1,3 @@
+import { StateSchema } from '@/global/providers/StoreProvider'
+
+export const getCurrentTrackTimeLenght = (state: StateSchema) => state.player.currentTrackTimeLenght
---
+++ src/entity/player/model/selectors/getPlaylist.ts
@@ -0,0 +1,3 @@
+import { StateSchema } from '@/global/providers/StoreProvider'
+
+export const getPlaylist = (state: StateSchema) => state.player.list
