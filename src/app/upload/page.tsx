2c3c161cdb4ae5f68a27cc77e6ac4cce7d121f40
Maxich <wimer2630@gmail.com>
2023-08-27 22:39:27 +0400

    upload page

---
+++ src/app/upload/page.tsx
@@ -0,0 +1,16 @@
+import { AddNewTrack } from '@/feature/AddNewTrack';
+import Logo from '@/shared/assets/svg/Logo.svg'
+import { Text } from '@/shared/ui/Text/Text';
+import cls from './upload.module.scss'
+
+export default function addTrack () {
+    return (
+        <>
+        <div className={cls.logo}>
+          <Logo/>
+        </div>
+        <Text title align='center' className={cls.title}>Загрузить трек</Text>
+        <AddNewTrack/>
+      </>
+    )
+}
