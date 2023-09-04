9314f34adc12562d82a240e93d41d7694f2eb65b
Maxich <wimer2630@gmail.com>
2023-09-04 22:48:09 +0400

    Story case for SignInFeature

---
+++ src/feature/SignIn/ui/Signin/SignIn.stories.tsx
@@ -0,0 +1,39 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { SignIn } from './Signin'
+import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
+
+const meta = {
+	title: 'features/SignIn',
+	component: SignIn,
+	parameters: {
+		layout: 'centered',
+	},
+	argTypes: { onClick: { action: 'clicked' } },
+	tags: ['autodocs'],
+} satisfies Meta<typeof SignIn>
+
+export default meta
+type Story = StoryObj<typeof meta>;
+
+const args: Story['args'] = {
+	
+}
+
+export const Default: Story = {
+	args
+}
+Default.decorators = [
+    StoreDecorator({})
+]
+
+export const Error: Story = {
+	args
+}
+Error.decorators = [
+    StoreDecorator({
+        signin: {
+            errorMessage: 'Неправильные данные для входа',
+            email: 'asdfasdfasdf'
+        }
+    })
+]
