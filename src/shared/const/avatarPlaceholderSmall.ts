25ddfb3b8ea7a9aeb494e7c79c2312ee8dae9707
Maxich <wimer2630@gmail.com>
2023-09-17 01:42:08 +0400

    avatarPlaceholder

--- src/shared/const/avatarPlaceholderSmall.ts
+++ src/shared/const/avatarPlaceholderSmall.ts
@@ -1,3 +1,5 @@
 import { backendUrl } from './backendUrl';
 
 export const avatarPlaceholderSmall = backendUrl + '/avatars/AvatarPlaceholder_small.jpg'
+
+export const avatarPlaceholder = backendUrl + '/avatars/AvatarPlaceholder.jpg'
