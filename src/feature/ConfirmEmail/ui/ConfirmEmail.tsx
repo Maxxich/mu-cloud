dce5daa9cbf896cd2f9f3394432547912625642b
Maxich <wimer2630@gmail.com>
2023-10-01 22:24:48 +0400

    ConfirmEmail feature

---
+++ src/feature/ConfirmEmail/ui/ConfirmEmail.tsx
@@ -0,0 +1,43 @@
+'use client'
+import classNames from 'classnames'
+import toast from 'react-hot-toast';
+import { memo, useCallback } from 'react'
+import { Form } from '@/shared/ui/Form/Form';
+import { Button } from '@/shared/ui/Button/Button';
+import { userRtkApi } from '@/entity/user';
+import { Text } from '@/shared/ui/Text/Text';
+import cls from './ConfirmEmail.module.scss'
+
+interface ConfirmEmailProps {
+    className?: string;
+    id?: string
+}
+
+export const ConfirmEmail = memo((props: ConfirmEmailProps) => {
+    const { className, id } = props
+    const classes = classNames(cls.container, {}, className)
+    const [triggerSend] = userRtkApi.useSendEmailConfirmationMutation()
+
+    const onClick = useCallback(async () => {
+        try {
+            await triggerSend().unwrap()
+            toast('Ссылка для потверждения отправлена на ваш Email')
+        } catch (error) {
+            toast('Ошибка')
+        }
+    }, [triggerSend])
+    
+    return (
+        <Form.Box margin='auto' className={classes} id={id}>
+            <Text title>Почта</Text>
+            <Text>Ваша почта не подтверждена</Text>
+            <Button
+                onClick={onClick}
+            >
+                Отправить ссылку для подтверждения
+            </Button>
+        </Form.Box>
+    )
+})
+
+ConfirmEmail.displayName = 'ConfirmEmail'
