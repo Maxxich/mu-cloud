3ef3c9665600fc7edf7e5aec9b69cfd049373552
Maxich <wimer2630@gmail.com>
2023-10-10 23:36:18 +0400

    Shared ItemsTitle story cases

--- src/shared/ui/ItemsTitle/ItemsTitle.stories.tsx
+++ src/shared/ui/ItemsTitle/ItemsTitle.stories.tsx
@@ -5,7 +5,7 @@ const meta = {
     title: 'shared/ItemsTitle',
     component: ItemsTitle,
     parameters: {
-        layout: 'centered',
+        layout: 'padded',
     },
     tags: ['autodocs'],
 } satisfies Meta<typeof ItemsTitle>
---
+++ src/shared/ui/ItemsTitle/index.ts
@@ -0,0 +1,1 @@
+export { ItemsTitle } from './ItemsTitle'
