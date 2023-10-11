62df4d5bd8ece9158a46d766f49aff47c112468a
Maxich <wimer2630@gmail.com>
2023-10-11 18:58:31 +0400

    types for form validation in redux state

--- src/global/types/global.d.ts
+++ src/global/types/global.d.ts
@@ -18,4 +18,23 @@ type OptionalRecord<K extends keyof any, T> = {
 
 type Mods = Record<string, boolean | string | undefined>
 
+
+type ErrorArraysByFields = Record<string, string[]>
+
+type ErrrosFlags<T extends ErrorArraysByFields> = {
+    //@ts-ignore
+    [K in keyof T]: T[K] extends Array<infer U> ? Record<U, boolean> : never;
+}
+
+
+type ErrorsConfig<ValidationErrrosFlags> = {
+    [Field in keyof ValidationErrrosFlags]: {
+        messages: {
+            [Error in keyof ValidationErrrosFlags[Field]]: string
+        },
+        [key: string]: any
+    }
+} 
+
+
 declare module 'server-only'
