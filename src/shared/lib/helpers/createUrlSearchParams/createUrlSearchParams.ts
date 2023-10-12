910c9b06352ad6f37928e9c2582e2e04310b4cea
Maxich <wimer2630@gmail.com>
2023-10-13 00:02:33 +0400

    shared createUrlSearchParams()

--- src/shared/lib/helpers/createUrlSearchParams/createUrlSearchParams.ts
+++ src/shared/lib/helpers/createUrlSearchParams/createUrlSearchParams.ts
@@ -1,3 +1,10 @@
-export function createUrlSearchParams<T extends URLSearchParams>(params: T) {
-    return new URLSearchParams(params).toString()
+export function createUrlSearchParams(params: OptionalRecord<string, string | number>) {
+    const recordParams: Record<string, string> = {}
+    const keys = Object.keys(params)
+    for (let i in keys) {
+        const param = params[keys[i]]
+        if (param) recordParams[keys[i]] = param.toString()
+    }
+    
+    return new URLSearchParams(recordParams).toString()
 }
