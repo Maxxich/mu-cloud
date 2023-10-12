6c798f144ac905bc3f1727891d77ed5a122f255a
Maxich <wimer2630@gmail.com>
2023-10-12 23:55:27 +0400

    entity player

--- src/entity/player/lib/useObserveChangeSelectedTrack.ts
+++ src/entity/player/lib/useObserveChangeSelectedTrack.ts
@@ -1,18 +1,11 @@
 'use client'
 import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
+import { backendUrl } from '@/shared/const/backendUrl'
 import { useAppDispatch } from '@/global/providers/StoreProvider/config/store'
 import { useAudio } from '@/shared/lib/hooks/useAudio/useAudio'
 import { playerActions } from '../model/slices/playerSlice'
 import { getSelectedTrack } from '../model/selectors/getSelectedTrack/getSelectedTrack'
-
-// useEffect(() => {
-//     audio.src = 'http://localhost:5001/' + selectedTrack.audio_src
-//     console.log(audio)
-//     audio.play()
-// }, [selectedTrack, audio])
-
-
 
 export const useObserveChangeCurrentTrack = () => {
 
@@ -28,19 +21,15 @@ export const useObserveChangeCurrentTrack = () => {
             return 
         }
     
-        const audioSource = 'http://localhost:5001/' + selectedTrack.audio_src
+        const audioSource = backendUrl + '/' + selectedTrack.audio_src
         audio.src = audioSource
 
         navigator.mediaSession.metadata = new MediaMetadata({
             title: selectedTrack.name + ' ' + selectedTrack.name_secondary,
             artist: selectedTrack.owners[0].name,
             artwork: [
-                { src: 'http://localhost:5001/' + selectedTrack.picture_source.square,   sizes: '100x100',   type: 'image/png' },
-                //   { src: 'https://dummyimage.com/128x128', sizes: '128x128', type: 'image/png' },
-                //   { src: 'https://dummyimage.com/192x192', sizes: '192x192', type: 'image/png' },
-                //   { src: 'https://dummyimage.com/256x256', sizes: '256x256', type: 'image/png' },
-                //   { src: 'https://dummyimage.com/384x384', sizes: '384x384', type: 'image/png' },
-                { src:  'http://localhost:5001/' + selectedTrack.picture_source.square,   sizes: '800x800', type: 'image/png' },
+                { src: backendUrl + '/' + selectedTrack.picture_source.square,   sizes: '100x100',   type: 'image/png' },
+                { src:  backendUrl + '/' + selectedTrack.picture_source.square,   sizes: '800x800', type: 'image/png' },
             ]
         });
 
@@ -48,7 +37,7 @@ export const useObserveChangeCurrentTrack = () => {
         navigator.mediaSession.setActionHandler('previoustrack', () => dispatch(playerActions.setPreviusTrack()))
 
         audio.onloadeddata = () => {
-            fetch('http://localhost:5001/' + `tracks/register-listening/${selectedTrack.id}`)
+            fetch(backendUrl + `/tracks/register-listening/${selectedTrack.id}`)
             if (!audio) return
             dispatch(playerActions.overwriteCurrentTrackTimeLenght(audio.duration))
             audio.play()
