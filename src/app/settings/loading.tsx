ffe78ed3b6dd4044faa96020db51ec843cb6034d
Maxich <wimer2630@gmail.com>
2023-10-29 12:07:16 +0400

    fix loader

--- src/app/settings/loading.tsx
+++ src/app/settings/loading.tsx
@@ -1,16 +1,7 @@
-import Logo from '@/shared/assets/svg/Logo.svg'
-import { Text } from '@/shared/ui/Text';
 import { Loader } from '@/shared/ui/Loader';
-import cls from './Settings.module.scss'
 
 export default function AddNewTrackLoading() {
     return (
-        <>
-            <div className={cls.logo}>
-                <Logo/>
-            </div>
-            <Text title align='center' className={cls.title}>Настройки аккаунта</Text>
-            <Loader center/>
-        </>
+        <Loader center/>
     )
 }
--- src/app/tracks/[id]/loading.tsx
+++ src/app/tracks/[id]/loading.tsx
@@ -1,5 +1,14 @@
-import { Loader } from '@/shared/ui/Loader';
+import { Skeleton } from '@/shared/ui/Skeleton'
+import cls from './Loading.module.scss'
 
 export default function LoadingTracks () {
-    return <Loader/>
+    return (
+        <div className={cls.container}>
+            <div className={cls.track_card_inner}>
+                <Skeleton width={156} height={156} borderRadius='15%'/>
+                <Skeleton width={136} height={64} borderRadius='10px'/>
+            </div>
+            <Skeleton width={350} height={37} borderRadius='10px'/>
+        </div>
+    )
 }
