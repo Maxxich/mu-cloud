abb9c0336ddfc0f55b63589e2aff5c93d8607338
Maxich <wimer2630@gmail.com>
2023-11-02 16:59:14 +0400

    test withAuth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,16 @@
-export { default } from 'next-auth/middleware'
+import { withAuth } from 'next-auth/middleware'
 
-export const config = {
-    matcher: [
-        '/upload/:path*',
-        '/library/:path*',
-        '/settings/:path*'
-    ]
-}
+
+export default withAuth({
+    callbacks: {
+        authorized({ req, token }) {
+            return !!token
+        },
+    },
+})
+
+export const config = { matcher: [
+    '/upload/:path*',
+    '/library/:path*',
+    '/settings/:path*'
+] }
