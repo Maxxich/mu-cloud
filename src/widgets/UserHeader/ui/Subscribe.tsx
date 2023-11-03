e553911ef75d129219b8d311164830e926c41a58
Maxich <wimer2630@gmail.com>
2023-11-03 09:32:26 +0400

    fix UserHeader subscribe

--- src/widgets/UserHeader/ui/Subscribe.tsx
+++ src/widgets/UserHeader/ui/Subscribe.tsx
@@ -1,7 +1,9 @@
 'use client'
-import { useCallback } from 'react'
+import { useSession } from 'next-auth/react';
+import { useCallback, useState } from 'react'
 import { userRtkApi } from '@/entity/user';
 import { Button } from '@/shared/ui/Button';
+import { RequireAuthDialog } from '@/shared/ui/RequireAuthDialog';
 
 interface ISubscribeProps {
     className?: string
@@ -13,24 +15,41 @@ export const Subscribe: React.FunctionComponent<ISubscribeProps> = ({
     id
 }) => {
 
+    const session = useSession()
+
     const { data: subscribed, isError } = userRtkApi.useCheckSubscribeQuery({ id })
     const [triggerCreate] = userRtkApi.useCreateSubscribeMutation()
     const [triggerRemove] = userRtkApi.useRemoveSubscribeMutation()
 
+    const [isOpen, setIsOpen] = useState(false)
+
+    const onClose = () => {
+        setIsOpen(false)
+    }
+
     const onClick = useCallback(() => {
+        if (session.status !== 'authenticated') {
+            return setIsOpen(true)
+        }
         if (isError) return
         if (subscribed) {
             triggerRemove({ id })
         } else {
             triggerCreate({ id })
         }
-    }, [triggerRemove, triggerCreate, isError, subscribed, id])
+    }, [triggerRemove, triggerCreate, isError, subscribed, id, session])
 
     const text = subscribed ? 'Вы подписаны' : 'Подписаться'
 
     return (
-        <Button className={className} onClick={onClick}>
-            {text}
-        </Button>
+        <>
+            <Button className={className} onClick={onClick}>
+                {text}
+            </Button>
+            <RequireAuthDialog
+                open={isOpen}
+                onClose={onClose}
+            />
+        </>
     );
 };
