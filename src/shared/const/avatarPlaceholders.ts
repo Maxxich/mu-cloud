99e37bdf4129000274717f725f2a86cd8d1b98d1
Maxich <wimer2630@gmail.com>
2023-11-05 19:17:54 +0400

    placeholder fix

--- src/shared/const/avatarPlaceholders.ts
+++ src/shared/const/avatarPlaceholders.ts
@@ -1,5 +1,8 @@
-import { backendUrl } from './backendUrl';
+// import { backendUrl } from './backendUrl';
+import avatar from '@/shared/assets/images/AvatarPlaceholder.jpg'
+import avatar_small from '@/shared/assets/images/AvatarPlaceholder_small.jpg'
+// export const avatarPlaceholderSmall = backendUrl + '/avatars/AvatarPlaceholder_small.jpg'
+export const avatarPlaceholderSmall = avatar.src
 
-export const avatarPlaceholderSmall = backendUrl + '/avatars/AvatarPlaceholder_small.jpg'
-
-export const avatarPlaceholder = backendUrl + '/avatars/AvatarPlaceholder.jpg'
+// export const avatarPlaceholder = backendUrl + '/avatars/AvatarPlaceholder.jpg'
+export const avatarPlaceholder = avatar_small.src
